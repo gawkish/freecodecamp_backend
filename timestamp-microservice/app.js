7bbe9fa1384c386e403847ddb7a2724426bbf47a
mmaskoliunas <gawk88@gmail.com>
2016-06-09 22:06:30 +0300

    Using $PORT for stating express

--- timestamp-microservice/app.js
+++ timestamp-microservice/app.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/:time', require('./routes/time'));
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log('App is now listening');
 });
 module.exports = app;
